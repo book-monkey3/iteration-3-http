9088eb6769b79c0d791d58653a64ca16ae78f4c6
The Buildbot <buildbot@angular2buch.de>
2018-12-30 22:24:51 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/8b51ea82f4a6b1c0954ddcb7c725e4c28321f834
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/1704

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Book } from '../shared/book';
-import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../../shared/book';
+import { BookStoreService } from '../../shared/book-store.service';
 
 @Component({
   selector: 'bm-book-list',
@@ -14,6 +14,6 @@ export class BookListComponent implements OnInit {
   constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
-    this.bs.getAll().subscribe(res => this.books = res); 
+    this.bs.getAll().subscribe(res => this.books = res);
   }
 }
